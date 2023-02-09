95366b0417e4823160f0f3b9e1b257b7ba9c1279
Troy Johnson <tnj8510@gmail.com>
2023-02-09 00:06:15 -0800

    add modal

---
+++ public/script/script.js
@@ -0,0 +1,14 @@
+document.getElementById("add_comment").addEventListener("click", function (){
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+modal.style.display = "block";
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+})
