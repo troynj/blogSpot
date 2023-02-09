5951ff64ab217f1e478afd6df6d3892576c91f19
Troy Johnson <tnj8510@gmail.com>
2023-02-09 09:20:25 -0800

    cant frontend fetch

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,10 @@
 // const Comment = require("./Comment.js")
 
-document.getElementById("add_comment").addEventListener("click", function (){
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
+document.getElementById("add_comment").addEventListener("click", function () {
+  var modal = document.getElementById("commentModal");
+  var span = document.getElementsByClassName("close")[0];
 
-modal.style.display = "block";
+  modal.style.display = "block";
   span.onclick = function () {
     modal.style.display = "none";
   };
@@ -13,16 +13,37 @@ modal.style.display = "block";
       modal.style.display = "none";
     }
   };
-})
+});
 
-document.getElementById("submit_comment").addEventListener("click", function(){
-  
-  console.log("in function")
-  const comment = document.getElementById("comment_input").value
-  
-  const userId = 1
-  const blogId = 1
-  const myComment = new Comment(comment, userId, blogId)
-  myComment.createComment()
+async function addComment(event) {
+  event.preventDefault();
 
-})
+  const commentEl = document.getElementById("comment_input").value;
+  const userId = 1;
+  const blogId = document.getElementsByClassName("blog__content").id;
+  const comment = { content: commentEl, user_id: userId, blog_id: blogId };
+
+  console.log("adding new comment");
+  console.log(comment);
+
+  const response = await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(comment),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    console.log("Comment added successfully:", data);
+  } else {
+    console.error("Error adding comment:", response.statusText);
+  }
+
+  document.location.reload();
+}
+
+document
+  .getElementById("add_comment_form")
+  .addEventListener("form", addComment);
